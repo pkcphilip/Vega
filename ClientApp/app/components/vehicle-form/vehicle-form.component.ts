25a50f087dd761c457ca30ce007798627f30d353
Philip Pang <pkcphilip@outlook.com>
2018-10-17 17:57:59 +0800

    Added Angular CLI and also feature. Implemented first dropdown for Make

---
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,21 @@
+import { MakeService } from './../../services/make.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent implements OnInit {
+  makes:any;
+
+  constructor(private makeService: MakeService) { }
+
+  ngOnInit() {
+    this.makeService.getMakes().subscribe(makes => {
+      this.makes = makes
+      console.log("MAKES", this.makes);
+    });
+  }
+
+}
