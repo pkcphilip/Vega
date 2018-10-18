6e27cb6d488b232fe45365a38cf704d9bb50a7c5
Philip Pang <pkcphilip@outlook.com>
2018-10-18 11:02:58 +0800

    Implemented Make, Model and Feature service and default vehicle form.

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,3 +1,4 @@
+import { FeatureService } from './../../services/feature.service';
 import { MakeService } from './../../services/make.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,15 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
-  makes:any;
+  makes:any[] = [];
+  models:any[] = [];
+  features:any[] = [];
+  vehicle:any = {};
 
-  constructor(private makeService: MakeService) { }
+  constructor(
+    private makeService: MakeService, 
+    private featureService: FeatureService) { }
 
   ngOnInit() {
-    this.makeService.getMakes().subscribe(makes => {
-      this.makes = makes
-      console.log("MAKES", this.makes);
-    });
+    this.makeService
+      .getMakes()
+      .subscribe(makes => {
+        this.makes = makes;
+        console.log(this.makes);
+      });
+
+    this.featureService
+      .getFeatures()
+      .subscribe(features => this.features = features);
   }
 
+  onMakeChange() {
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
+  }
 }
--- Migrations/20181017031935_SeedDatabase.cs
+++ Migrations/20181017031935_SeedDatabase.cs
@@ -14,13 +14,13 @@ namespace vega.Migrations
             migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelB', (SELECT ID FROM MAKES WHERE NAME = 'Make1'))");
             migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelC', (SELECT ID FROM MAKES WHERE NAME = 'Make1'))");
 
-            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelA', (SELECT ID FROM MAKES WHERE NAME = 'Make2'))");
-            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelB', (SELECT ID FROM MAKES WHERE NAME = 'Make2'))");
-            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelC', (SELECT ID FROM MAKES WHERE NAME = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelA', (SELECT ID FROM MAKES WHERE NAME = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelB', (SELECT ID FROM MAKES WHERE NAME = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelC', (SELECT ID FROM MAKES WHERE NAME = 'Make2'))");
 
-            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelA', (SELECT ID FROM MAKES WHERE NAME = 'Make3'))");
-            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelB', (SELECT ID FROM MAKES WHERE NAME = 'Make3'))");
-            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelC', (SELECT ID FROM MAKES WHERE NAME = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelA', (SELECT ID FROM MAKES WHERE NAME = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelB', (SELECT ID FROM MAKES WHERE NAME = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelC', (SELECT ID FROM MAKES WHERE NAME = 'Make3'))");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
