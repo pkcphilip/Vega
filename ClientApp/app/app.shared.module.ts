241447983dd765f6bd723f1b586c281b48b60924
Philip Pang <pkcphilip@outlook.com>
2018-11-05 18:00:17 +0800

    Installed Raven

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -1,3 +1,4 @@
+import * as Raven from 'raven-js';
 import { AppErrorHandler } from './app.error-handler';
 import { ErrorHandler } from '@angular/core';
 import { VehicleService } from './services/vehicle.service';
@@ -14,6 +15,8 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+
+Raven.config('https://fb9ef4041ec54f158fb36445428869c9:1a5c3fb1d7d54a2e824f07ab5f8341ea@sentry.io/1315798').install();
 
 @NgModule({
     declarations: [
